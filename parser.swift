f63829312f28deda6b751c4787ccbf3930ffc279
Peter Bouda <hey@peterbouda.eu>
2019-10-15 17:38:51 +0200

    Add postfix function.

--- parser.swift
+++ parser.swift
@@ -17,7 +17,59 @@ func insertExplicitConcatOperator(exp: String) -> String {
     return output
 }
 
+func peek<T>(stack: Array<T>) -> T? {
+    if (!stack.isEmpty) {
+        return stack[stack.count - 1]
+    } 
+    return nil
+} 
+
+func operatorPrecedence(op: Character) -> Int {
+    let opPrecedence: [Character: Int] = [
+        "|": 0,
+        ".": 1,
+        "*": 2
+    ]
+    let result = opPrecedence[op]
+    if result != nil {
+        return result!
+    }
+    return -1
+}
+
+func toPostfix(exp: String) -> String {
+    var output = ""
+    var operatorStack: [Character] = [];
+
+    for token in exp {
+        if token == "." || token == "|" || token == "*" {
+            while let latestOp = peek(stack: operatorStack), latestOp != "(" &&
+                    operatorPrecedence(op: latestOp) >= operatorPrecedence(op: token) {
+                output += String(operatorStack.removeLast())
+            }
+            operatorStack.append(token)
+        } else if token == "(" || token == ")" {
+            if token == "(" {
+                operatorStack.append(token)
+            } else {
+                while let latestOp = peek(stack: operatorStack), latestOp != "(" {
+                    output += String(operatorStack.removeLast()) 
+                }
+                operatorStack.removeLast()
+            }
+        } else {
+            output += String(token)
+        }
+    }
+    while !operatorStack.isEmpty {
+        output += String(operatorStack.removeLast())
+    }
+    return output
+}
+
 let regex = "a(b|c)*"
 let result = insertExplicitConcatOperator(exp: regex)
 print(result)
+let result2 = toPostfix(exp: result)
+print(result2)
 
